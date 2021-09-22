d3ed2492f29a8be77793991518f3b7ec9e871ad3
masaya <spag9hc9@yahoo.co.jp>
2021-09-22 10:19:27 +0900

    test pr

--- front/topicslab/src/views/Home.vue
+++ front/topicslab/src/views/Home.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="home">
-    <div class="top-logo">
-      <img alt="logo" src="../assets/logo.png" />
-      <span class="message">社会課題について、みんなで話し合おう！？</span>
+  <div class='home'>
+    <div class='top-logo'>
+      <img alt='logo' src='../assets/logo.png' />
+      <span class='message'>社会課題について、みんなで話し合おう!?</span>
     </div>
     <AllTopics />
   </div>
 </template>
 
 <script>
-import AllTopics from "@/components/AllTopics";
+import AllTopics from '@/components/AllTopics'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     AllTopics
   }
-};
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 .top-logo {
   display: block;
   margin: 0 auto;
